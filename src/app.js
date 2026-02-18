c883b5949c7bab9403c04147a67b010ab8d0ab1d
AbhijitDevghare <kishordevghare6@gmail.com>
2026-02-18 15:31:37 +0530

    origin removed

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS
 app.use(cors({
-  origin: [process.env.FRONTEND_ORIGIN,"http://localhost:5174"],
+  origin: process.env.FRONTEND_ORIGIN,
   credentials: true,
 }));
 
